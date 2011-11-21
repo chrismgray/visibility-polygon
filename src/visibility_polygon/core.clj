eff9b0fe0241eb9578ca5a7e6bea2172d00b72fb
Chris Gray <chrismgray@gmail.com>
2011-11-21 00:15:27 -0700

    Changed the monad so that the poly is the basic value.
    This allows me to use the poly as a stopping condition.  When it is
    empty, then I can stop.  However, it makes the parsing functions a bit
    more complicated.  Oh well.

--- src/visibility_polygon/core.clj
+++ src/visibility_polygon/core.clj
@@ -5,41 +5,51 @@
 
 
 (defmonad polygon-parser-m
-  [m-result (fn [v]
-              (fn [pt poly stack]
+  [m-result (fn [poly]
+              (fn [pt stack]
                 [pt (vec poly) (vec stack)]))
    m-bind (fn [mv f]
-            (fn [pt poly stack]
-              (let [result (mv pt poly stack)]
+            (fn [pt stack]
+              (let [result (mv pt stack)]
                 (when (not= nil result)
                   (let [[pt new-poly new-stack] result]
-                    ((f nil) pt new-poly new-stack))))))
-   m-zero (fn [pt poly stack]
+                    ((f new-poly) pt new-stack))))))
+   m-zero (fn [pt stack]
             nil)
    m-plus (fn [& parsers]
-            (fn [pt poly stack]
+            (fn [pt stack]
               (drop-while nil?
-                          (map #(% pt poly stack) parsers))))])
+                          (map #(% pt stack) parsers))))])
 
 (def visible? (complement pt/left-turn?))
 
-(defn add-new-pt [pt poly stack]
-  (when (visible? pt (first poly) (first stack))
-    [pt (rest poly) (vec (cons (first poly) stack))]))
+(defn add-new-pt [poly]
+  (fn [pt stack]
+    (when (visible? pt (first poly) (first stack))
+      [pt (rest poly) (vec (cons (first poly) stack))])))
 
-(defn pop-stack [pt poly stack]
-  (let [the-seg (seg/new-seg pt (first poly))
-        top-seg (seg/new-seg (first stack) (second stack))]
-    (when (pt/left-turn? (second stack) (first stack) (first poly))
-      (if (intersection-on-seg? the-seg top-seg)
-        [pt poly (cons (seg/intersection the-seg top-seg) stack)]
-        [pt poly (rest stack)]))))
+(defn pop-stack [poly]
+  (fn [pt stack]
+    (let [the-seg (seg/new-seg pt (first poly))
+          top-seg (seg/new-seg (first stack) (second stack))]
+      (when (pt/left-turn? (second stack) (first stack) (first poly))
+        (if (seg/intersection-on-seg? the-seg top-seg)
+          [pt poly (cons (seg/intersection the-seg top-seg) stack)]
+          [pt poly (rest stack)])))))
 
-(defn skip-pt [pt poly stack]
-  (let [the-seg (seg/new-seg pt (first stack))
-        poly-seg (seg/new-seg (first poly) (second poly))]
-    (when (not (pt/left-turn? (second stack) (first stack) (first poly)))
-      (if (intersection-on-seg? the-seg poly-seg)
-        [pt (cons (seg/intersection the-seg poly-seg) (rest poly)) stack]
-        [pt (rest poly) stack]))))
+(defn skip-pt [poly]
+  (fn [pt stack]
+   (let [the-seg (seg/new-seg pt (first stack))
+         poly-seg (seg/new-seg (first poly) (second poly))]
+     (when (not (pt/left-turn? (second stack) (first stack) (first poly)))
+       (if (seg/intersection-on-seg? the-seg poly-seg)
+         [pt (cons (seg/intersection the-seg poly-seg) (rest poly)) stack]
+         [pt (rest poly) stack])))))
 
+(defn all-conditions [poly]
+  (with-monad polygon-parser-m
+    (m-plus (add-new-pt poly) (pop-stack poly) (skip-pt poly))))
+
+(defn visibility-polygon-helper [pt poly]
+  (with-monad polygon-parser-m
+    (m-until #(seq %) all-conditions poly)) pt [])
